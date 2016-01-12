bd53729610db29cd7c231b5391b6beea40e1d94d
Mario López Baratas <mariolopez000@hotmail.com>
2016-01-13 00:35:51 +0100

    Template changed for UWP support

--- XamFormsMarvel/Views/FirstView.xaml.cs
+++ XamFormsMarvel/Views/FirstView.xaml.cs
@@ -8,39 +8,78 @@ using Xamarin.Forms.Xaml;
 
 namespace XamFormsMarvel.Views
 {
-	[XamlCompilation (XamlCompilationOptions.Compile)]
-	public partial class FirstView : ContentPage
-	{
-		private FirstViewModel _vm;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstView : ContentPage
+    {
+        private FirstViewModel _vm;
 
-		public FirstView ()
-		{
-			InitializeComponent ();
-			_vm = new FirstViewModel ();
+        public FirstView()
+        {
+            InitializeComponent();
 
-			// Navigation to detail page
-			this.listCharacters.ItemSelected += (object sender, SelectedItemChangedEventArgs e) => {
-				var character = (CharacterItemViewModel)e.SelectedItem;
-				var detailVm = new DetailViewModel(character);
+            //var image = new Image()
+            //{
+            //    HeightRequest = 80,
+            //    WidthRequest = 80
+            //};
+            //image.SetBinding(Image.SourceProperty, "Thumbnail");
+            //var label = new Label()
+            //{
+            //    VerticalTextAlignment = TextAlignment.Start,
+            //    HorizontalTextAlignment = TextAlignment.Start,
+            //    VerticalOptions = LayoutOptions.FillAndExpand,
+            //    HorizontalOptions = LayoutOptions.FillAndExpand
+            //};
+            //label.SetBinding(Label.TextProperty, "Name");
+            //var stack = new StackLayout()
+            //{
+            //    Orientation = StackOrientation.Horizontal
+            //};
+            //stack.Children.Add(image);
+            //stack.Children.Add(label);
 
-				var detailView = new DetailView(detailVm);
+            //var dt = new DataTemplate(() =>
+            //{
+            //    return new ViewCell()
+            //    {
+            //        View = stack
+            //    };
+            //});  
 
-				this.Navigation.PushAsync(detailView);
-			};
+            var dt = new DataTemplate(typeof(ImageCell));
+            dt.SetBinding(ImageCell.ImageSourceProperty, "Thumbnail");
+            dt.SetBinding(ImageCell.TextProperty, "Name");
 
-//			Label lbl = new Label ();
-//			lbl.HorizontalTextAlignment = TextAlignment.Start
+            listCharacters.ItemTemplate = dt;
 
-			BindingContext = _vm;
-			_vm.LoadData();
-		}
+            listCharacters.SetBinding(ListView.ItemsSourceProperty, new Binding("CharacterList"));
+
+            _vm = new FirstViewModel();
+
+            // Navigation to detail page
+            this.listCharacters.ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
+            {
+                var character = (CharacterItemViewModel)e.SelectedItem;
+                var detailVm = new DetailViewModel(character);
+
+                var detailView = new DetailView(detailVm);
+
+                this.Navigation.PushAsync(detailView);
+            };
+
+            //			Label lbl = new Label ();
+            //			lbl.HorizontalTextAlignment = TextAlignment.Start
+
+            BindingContext = _vm;
+            _vm.LoadData();
+        }
 
 
-		protected override async void OnAppearing ()
-		{
-			base.OnAppearing ();
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
 
-		}
-	}
+        }
+    }
 }
 
