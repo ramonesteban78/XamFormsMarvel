b3d22319f33956cb7b91251353d96fda45a8dfde
Ramon Esteban <ramonesteban78@gmail.com>
2016-01-15 12:02:19 +0100

    DependencyService Open Web site

---
+++ Droid/Services/OpenWebService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using XamFormsMarvel.Droid.Services;
+using XamFormsMarvel.Services;
+using Android.Content;
+using Android.App;
+
+
+[assembly: Xamarin.Forms.Dependency (typeof (OpenWebService))]
+namespace XamFormsMarvel.Droid.Services
+{
+	public class OpenWebService : IOpenWebService
+	{
+		#region IOpenWebService implementation
+
+		public void OpenUrl (string url)
+		{
+			var uri = Android.Net.Uri.Parse (url);
+			var intent = new Intent (Intent.ActionView, uri); 
+			intent.SetFlags (ActivityFlags.NewTask);
+			Application.Context.StartActivity (intent);
+		}
+
+		#endregion
+	}
+}
+
---
+++ XamFormsMarvel/Services/IOpenWebService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace XamFormsMarvel.Services
+{
+	public interface IOpenWebService
+	{
+		void OpenUrl(string url);
+	}
+}
+
