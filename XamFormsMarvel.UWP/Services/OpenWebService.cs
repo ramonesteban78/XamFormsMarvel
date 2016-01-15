c9e106b56538c0be4be8590747a35b35fa527190
Mario López Baratas <mariolopez000@hotmail.com>
2016-01-15 12:34:26 +0100

    Project fixed and webservice UWP

---
+++ XamFormsMarvel.UWP/Services/OpenWebService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamFormsMarvel.UWP.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(OpenWebService))]
+namespace XamFormsMarvel.UWP.Services
+{
+    public class OpenWebService
+    {
+        public void OpenUrl(string url)
+        {
+
+        }
+    }
+}
